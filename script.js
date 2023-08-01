3e56e6c03bc420e138599579d11473ca0705cdc7
mah <mahsa.79mirmiran@gmail.com>
2023-08-01 15:10:02 +0300

    wooow i set lots of shooting star on header section

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
         var collapsibleElement = document.getElementById(target);
         collapsibleElement.classList.toggle('hidden');
     });
+
+    function animateMeteorite() {
+    const meteorite = document.querySelector('.meteorite');
+    const bounding = meteorite.getBoundingClientRect();
+
+    // Check if the .meteorite element is in the viewport
+    if (bounding.top <= window.innerHeight) {
+      meteorite.classList.add('animate-move-left-and-right');
+    }
+  }
+
+  // Add an event listener to the scroll event to trigger the animation function
+  window.addEventListener('scroll', animateMeteorite);
+
+  // Trigger the animation function when the page loads
+  animateMeteorite();
 });
