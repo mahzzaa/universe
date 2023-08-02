5b1c1f44bca3f23a5dcaf7b9c8592e35882a4549
mah <mahsa.79mirmiran@gmail.com>
2023-08-02 18:40:15 +0300

    uf !:/ fixed navbar responsive

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('hamburger-button').addEventListener('click', function() {
-        var target = this.getAttribute('data-collapse-target');
-        var collapsibleElement = document.getElementById(target);
-        collapsibleElement.classList.toggle('hidden');
+    const menuIcon = document.getElementById('menuIcon');
+    const navLink = document.querySelector('.nav-link');
+    const openSvg = document.getElementById('open-svg');
+    const closeSvg = document.getElementById('close-svg');
+
+
+
+    // menuIcon.addEventListener('click', function () {
+    //     this.name = this.name === 'menu' ? 'close' : 'menu';
+    //     navLink.classList.toggle('hidden');
+    // });
+
+
+    openSvg.addEventListener('click', function () {
+        openSvg.classList.toggle('hidden');
+        closeSvg.classList.toggle('hidden');
+        navLink.classList.toggle('hidden');
     });
 
-    function animateMeteorite() {
-    const meteorite = document.querySelector('.meteorite');
-    const bounding = meteorite.getBoundingClientRect();
+    closeSvg.addEventListener('click', function () {
+        openSvg.classList.toggle('hidden');
+        closeSvg.classList.toggle('hidden');
+        navLink.classList.toggle('hidden');
+    });
 
-    // Check if the .meteorite element is in the viewport
-    if (bounding.top <= window.innerHeight) {
-      meteorite.classList.add('animate-move-left-and-right');
-    }
-  }
 
-  // Add an event listener to the scroll event to trigger the animation function
-  window.addEventListener('scroll', animateMeteorite);
 
-  // Trigger the animation function when the page loads
-  animateMeteorite();
 });
